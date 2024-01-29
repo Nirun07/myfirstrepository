ffea0d5ba99247b368deafb07cbba0e27c23a296
Nirun07 <nirun82@gmail.com>
2024-01-29 14:15:14 -0500

    myfirstfile

---
+++ celciustofaren2.py
@@ -0,0 +1,10 @@
+def celtofar ():
+  cel1 = int(input("enter celsius value: "))
+  far1=(cel1*9/5)+32
+  print ("farenheit value is", far1)
+  print (f"celsius value {cel1} is equal to farenheit value {far1}")
+
+
+celtofar()
+
+
